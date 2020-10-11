56013e67c0e59e23c767efb037583c49ec02edf7
Fabio Baser <git@fabiobaser.de>
2020-10-12 01:06:15 +0200

    Basic Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Input, Button } from "semantic-ui-react"
+import { Input, Button, Card, Container, Header } from "semantic-ui-react"
 import { HubConnectionBuilder } from "@microsoft/signalr"
 
 export default class App extends Component {
@@ -49,20 +49,57 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <Container textAlign='center' style={{ paddingTop: "4rem" }}>
+        <Header as='h1'>Wilkommen bei Ultimate Trivia</Header>
+        <p>Gib als erstes deinen Namen ein</p>
         <Input
           name='name'
           placeholder='Dein Name'
           onChange={this.handleInputChange}
         />
-        <Input
-          name='lobbyId'
-          placeholder='Einladungs Code'
-          onChange={this.handleInputChange}
-        />
-        <Button onClick={this.createLobby}>Lobby erstellen</Button>
-        <Button onClick={this.joinLobby}>Spiel beitreten</Button>
-      </div>
+
+        {this.state.name !== "" && (
+          <Card.Group centered style={{ marginTop: "2rem" }} disabled>
+            <Card>
+              <Card.Content>
+                <Card.Header>Spiel erstellen</Card.Header>
+                <Card.Description>
+                  Erstelle ein Spiel dem deine Freunde beitreten können
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <Button basic fluid onClick={this.createLobby}>
+                  Erstellen
+                </Button>
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <Card.Header>Spiel beitreten</Card.Header>
+                <Card.Description>
+                  Wenn ein Freund ein Spiel erstellt kannst du hier den Code
+                  eingeben und dem Spiel beitreten. <b>Viel Spaß!</b>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <Input
+                  name='lobbyId'
+                  fluid
+                  placeholder='Einladungs Code'
+                  onChange={this.handleInputChange}
+                  action={{
+                    children: "Beitreten",
+                    color: "green",
+                    basic: true,
+                    onClick: this.joinLobby,
+                  }}
+                />
+              </Card.Content>
+            </Card>
+          </Card.Group>
+        )}
+      </Container>
     )
   }
 }
