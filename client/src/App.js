bd7073b208b5f0990d12ed950f2eb5860e45d473
Fabio Baser <git@fabiobaser.de>
2020-10-11 23:25:01 +0200

    Added three inputs … yay

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,58 @@
 import React, { Component } from "react"
+import { Input, Button } from "semantic-ui-react"
+import { HubConnectionBuilder } from "@microsoft/signalr"
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: "",
+      lobbyId: "",
+    }
+  }
+
+  componentDidMount() {
+    this.connectToHub()
+  }
+
+  connectToHub = () => {
+    const connection = new HubConnectionBuilder().withUrl("/").build()
+
+    connection.on("send", (data) => {
+      console.log(data)
+    })
+
+    connection.start().then(() => connection.invoke("send", "Hello"))
+  }
+
+  handleInputChange = (e, props) => {
+    const { name, value } = props
+    const newState = this.state
+    newState[name] = value
+    this.setState(newState)
+  }
+
+  connectToLobby = () => {
+    const { lobbyId } = this.state
+    console.log("%cConnecting to code: ", "color: orange", lobbyId)
+  }
+
   render() {
-    return <h1>Hello</h1>
+    return (
+      <div>
+        <Input
+          name='name'
+          placeholder='Dein Name'
+          onChange={this.handleInputChange}
+        />
+        <Input
+          name='lobbyId'
+          placeholder='Einladungs Code'
+          onChange={this.handleInputChange}
+        />
+        <Button onClick={this.connectToLobby}>Spiel beitreten</Button>
+      </div>
+    )
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,6 @@ import React from "react"
 import { render } from "react-dom"
 import App from "./App"
 
+import "semantic-ui-css/semantic.min.css"
+
 render(<App />, document.getElementById("app"))
