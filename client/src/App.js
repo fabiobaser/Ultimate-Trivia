12492beda06c485bc73f540dacaa19a406c4d7df
MarcelJenner <marcel.jenner97@gmail.com>
2020-10-12 17:53:07 +0200

    remove debugger statements

--- client/src/App.js
+++ client/src/App.js
@@ -104,14 +104,11 @@ export default class App extends Component {
 
     this.connection.start()
         .then((resolve) => {
-          debugger;
           console.log(resolve)
         }, (reject) => {
-          debugger;
           console.log(reject)
         })
         .catch((error) => {
-          debugger;
         console.log(error);
     })
   }
